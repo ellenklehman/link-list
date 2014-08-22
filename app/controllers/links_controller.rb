daa5714bf35a4103ca4f28b67bc8a75b8338c7d0
Ellen Lehman <ellenklehman@users.noreply.github.com>
2014-08-22 12:35:13 -0700

    add create and show functionality to links model

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,27 @@
 class LinksController < ApplicationController
+
   def index
     @links = Link.all
     render('links/index.html.erb')
+  end
+
+  def new
+  	@link = Link.new
+  	render('links/new.html.erb')
+  end
+
+  def create
+  	@link = Link.new(params[:link])
+  	if @link.save
+  		flash[:notice] = "Your link was successully created."
+  		redirect_to("/links/#{@link.id}")
+  	else
+  		render('links/new.html.erb')
+  	end
+  end
+
+  def show
+  	@link = Link.find(params[:id])
+  	render('links/show.html.erb')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'links#index'
 
   match('/links', {:via => :get, :to => 'links#index'})
+  match('/links', {:via => :post, :to => 'links#create'})
+  match('/links/new', {:via => :get, :to => 'links#new'})
+  match('/links/:id', {:via => :get, :to => 'links#show'})
 end
