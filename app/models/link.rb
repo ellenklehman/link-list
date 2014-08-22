4821c4d9281ae7c68751fceee8424101e3f9a906
Ellen Lehman <ellenklehman@users.noreply.github.com>
2014-08-22 14:16:29 -0700

    add validation of url uniqueness to links model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,6 @@
 class Link < ActiveRecord::Base
 	validates :name, :presence => true
   validates :url, :presence => true
+  validates :url, :uniqueness => true
   belongs_to :student
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Link do
 	it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+  it { should validate_uniqueness_of :url }
   it { should belong_to :student }
 end
